6464c4e1e214ced0834dbb9f2c9656de957098f8
supri hadianto <bintanEMAIL@gmail.com>
2017-10-25 01:13:26 +0700

    Add socket.io & make event connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.send('Node Chat App');
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
 });
 
 
-app.listen(port, () => {
+
+// app.get('/', (req, res) => {
+//     res.send('Node Chat App');
+// });
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
