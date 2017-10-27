481db1c0078e4dd785bccfe15fc10abedafc7bef
supri hadianto <bintanEMAIL@gmail.com>
2017-10-28 01:07:29 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,7 +28,7 @@ socket.on('newLocationMessage', function (message) {
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    
+
     // jQuery create element
     jQuery('#messages').append(li);
 });
@@ -37,11 +37,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextBox.val()
     }, function () {
-        
+        messageTextBox.val('');
     });
 });
 
@@ -51,12 +53,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.')
     });
 });
