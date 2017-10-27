2907ac93e554f9a8a9bf73a7d766adfe5fa8a6bb
supri hadianto <bintanEMAIL@gmail.com>
2017-10-28 00:36:28 +0700

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -26,11 +26,10 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server');
-//         socket.broadcast.emit('newMessage', {
-//             from: message.from,
-//             text: message.text,
-//             createAt: new Date().getDate()
-//         });
+   });
+
+   socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
    });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 const expect = require('expect');
 const chai = require('chai');
 const should = chai.should();
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 
 describe('generateMessage', () => {
@@ -13,5 +13,18 @@ describe('generateMessage', () => {
         message.createdAt.should.that.is.a('number');
         message.should.to.include({from, text});
         
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate location object', () => {
+        var from = 'Toro';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://google.com/maps?q=15,19';
+        var location = generateLocationMessage(from, latitude, longitude);
+
+        location.createdAt.should.that.is.a('number');
+        location.should.to.include({from, url});
     });
 });
